baccebf5f544c3791db15bae9bd5aba94fbc9d0c
matebookWindows <dan.kovalev2013@gmail.com>
2024-10-19 13:06:47 +0300

    fix: type hints in service

--- app/logic/dependecies.py
+++ app/logic/dependecies.py
@@ -4,7 +4,7 @@ from typing import Type, TypeVar, Callable, Any
 T = TypeVar('T')
 
 
-def Service(service_type: Type[T]):
-    def resolve_service(request: Request) -> T:
+def Service(service_type: Type[T]) -> Any: # noqa: N802
+    def resolve_service(request: Request) -> Callable[[], T]:
         return request.app.state.ioc_container.resolve(service_type)
     return Depends(resolve_service)
